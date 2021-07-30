ed21cf74a8475f4dc370d5f3f2ee73963d6fb9b9
Mahmoud-Yasser-18 <s-mahmoudyasser@zewailcity.edu.eg>
2021-07-30 22:43:25 +0200

    refine requirements to .sh

---
+++ One-GPU/requirements.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install datasets==1.11.0
+pip install transformers==4.9.1
+pip install rouge-score==0.0.4
+pip install nltk==3.2.5 
+pip install deepspeed==0.4.4
