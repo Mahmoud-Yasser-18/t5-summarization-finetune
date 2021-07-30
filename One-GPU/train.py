e36b6d5c14c4f8e6b1e23fcd240fe16e44d152e5
Mahmoud-Yasser-18 <s-mahmoudyasser@zewailcity.edu.eg>
2021-07-30 23:12:29 +0200

    Adding lines before fine-tuning

--- One-GPU/train.py
+++ One-GPU/train.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python
+
+import datasets
+import random
+import transformers
+from datasets import load_dataset, load_metric
+import pandas as pd
+from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+
+model_checkpoint = "t5-small"
+
+raw_datasets = load_dataset("xsum")
+metric = load_metric("rouge")
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+if model_checkpoint in ["t5-small", "t5-base", "t5-larg", "t5-3b", "t5-11b"]:
+    prefix = "summarize: "
+else:
+    prefix = ""
+
+max_input_length = 1024
+max_target_length = 128
+
+def preprocess_function(examples):
+    inputs = [prefix + doc for doc in examples["document"]]
+    model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True)
+
+    # Setup the tokenizer for targets
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(examples["summary"], max_length=max_target_length, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+
+tokenized_datasets = raw_datasets.map(preprocess_function, batched=True)
+
