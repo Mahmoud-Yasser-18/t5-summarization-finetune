2283f8510418ee6322cec91bf1382f5c0ccfa4da
Mahmoud-Yasser-18 <s-mahmoudyasser@zewailcity.edu.eg>
2021-07-30 23:42:25 +0200

    Fine-tuning with out deepSpeed Matching

--- One-GPU/train.py
+++ One-GPU/train.py
@@ -38,3 +38,66 @@ def preprocess_function(examples):
 
 tokenized_datasets = raw_datasets.map(preprocess_function, batched=True)
 
+model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
+
+
+batch_size = 8
+model_name = model_checkpoint.split("/")[-1]
+args = Seq2SeqTrainingArguments(
+    "test-summarization",
+    overwrite_output_dir=True,
+    evaluation_strategy ='steps',
+    eval_steps = 10, # Evaluation and Save happens every 10 steps
+    save_total_limit = 1, # Only last 1 models are saved. Older ones are deleted.
+    load_best_model_at_end=True,
+    learning_rate=2e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    weight_decay=0.01,
+    num_train_epochs=1,
+    predict_with_generate=True,
+    fp16=True,
+    deepspeed="./deepspeed-zero2-one-gpu.json"
+)
+
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+
+import nltk
+import numpy as np
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    
+    # Rouge expects a newline after each sentence
+    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+    
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+    # Extract a few results
+    result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+    
+    # Add mean generated length
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+    result["gen_len"] = np.mean(prediction_lens)
+    
+    return {k: round(v, 4) for k, v in result.items()}
+
+
+trainer = Seq2SeqTrainer(
+    model,
+    args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+
+
+trainer.train()
