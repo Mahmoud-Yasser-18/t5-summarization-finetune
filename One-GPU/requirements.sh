3870501bad1b327a9f214b48338b6a2a3887fcdf
Mahmoud-Yasser-18 <s-mahmoudyasser@zewailcity.edu.eg>
2021-08-02 21:12:17 +0200

    gcc correct version

--- One-GPU/requirements.sh
+++ One-GPU/requirements.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo yum -y install gcc-c++
 if [ ! -f "/cuda_11.4.0_470.42.01_linux.run" ]; then
     wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda_11.4.0_470.42.01_linux.run
 fi
@@ -6,7 +7,7 @@ fi
 sudo sh cuda_11.4.0_470.42.01_linux.run
 
 pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
-
+pip install sentencepiece
 pip install datasets==1.11.0
 pip install pandas==1.1.5
 pip install git+https://github.com/huggingface/transformers
