859f10175bbe11dd9568c4aba0e4488387577190
Mahmoud-Yasser-18 <s-mahmoudyasser@zewailcity.edu.eg>
2021-08-02 17:45:31 +0200

    Updating requirements with CUDA

--- One-GPU/requirements.sh
+++ One-GPU/requirements.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux.run
-sudo sh cuda_11.0.2_450.51.05_linux.run
+wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda_11.4.0_470.42.01_linux.run
+sudo sh cuda_11.4.0_470.42.01_linux.run
+
 pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
 pip install datasets==1.11.0
 pip install pandas==1.1.5
 pip install git+https://github.com/huggingface/transformers
