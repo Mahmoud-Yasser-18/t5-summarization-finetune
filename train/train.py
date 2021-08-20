07087d7be20f8724359a99ccd2336cd651e53e8f
Mahmoud-Yasser-16 <mahmoud.yasser@corporatica.com>
2021-08-20 10:14:34 +0200

    removing the rouge

--- train/train.py
+++ train/train.py
@@ -154,8 +154,9 @@ trainer = Seq2SeqTrainer(
     train_dataset=tokenized_datasets["train"],
     eval_dataset=tokenized_datasets["validation"],
     data_collator=data_collator,
-    tokenizer=tokenizer,
-    compute_metrics=compute_metrics
+    # compute_metrics=compute_metrics,
+    tokenizer=tokenizer
+    
 )
 
 try:
